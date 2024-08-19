719710caefc7e82bed9f24e15aa1611a4284ede2
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-08-19 17:31:52 +0530

    Create RemoveDuplicatesFromSortedArray.java

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        for (int  i = 1 ; i < nums.length ; i++){
+            if (nums[i] != nums[i - 1]){
+                list.add(nums[i]);
+            }
+        }
+        for (int i = 0 ; i < list.size() ; i++){
+            nums[i] = list.get(i);
+        }
+        return list.size();
+    }
+}
