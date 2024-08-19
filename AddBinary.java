33984c8b3cebb526d75d49b26548e914f7878e03
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-08-19 23:14:35 +0530

    Create AddBinary.java

---
+++ AddBinary.java
@@ -0,0 +1,60 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1 , j = b.length()-1, carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i >= 0 && j >= 0){
+            if (a.charAt(i) == '1' && b.charAt(j) == '1'){
+                if (carry == 1)  sb.insert(0,"1");
+                else sb.insert(0,"0");
+                carry = 1;
+            }else if (a.charAt(i) == '0' && b.charAt(j) == '0'){
+                if (carry == 1)  sb.insert(0,"1");
+                else sb.insert(0,"0");
+                carry = 0;
+            }
+            else{
+                if (carry == 1){
+                    sb.insert(0,"0");
+                }  
+                else 
+                    sb.insert(0,"1");
+            }
+            i--;
+            j--;
+        }
+        while(i >= 0){
+            if (a.charAt(i) == '1'){
+                if (carry == 1)
+                    sb.insert(0,"0");
+                else 
+                    sb.insert(0,"1");
+            } else{
+                if (carry == 1)
+                    sb.insert(0,"1");
+                else 
+                    sb.insert(0,"0");
+                carry = 0;
+            }
+            i--;
+        }
+        while(j >= 0){
+            if (b.charAt(j) == '1'){
+                if (carry == 1)
+                    sb.insert(0,"0");
+                else {
+                    sb.insert(0,"1");
+                    carry = 0;
+                }
+            } else{
+                if (carry == 1)
+                    sb.insert(0,"1");
+                else 
+                    sb.insert(0,"0");
+                carry = 0;
+            }
+            j--;
+        }
+        if (carry == 1) sb.insert(0,"1");
+        return String.valueOf(sb);
+    }
+}
