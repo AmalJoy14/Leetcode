8d982d77b832f3b056b0b46ac5d6a1c8d9f1f7cd
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-09-16 23:51:04 +0530

    Create ReverseWordsInAString3.java

---
+++ ReverseWordsInAString3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ; i < arr.length ; i++){
+            StringBuilder a = new StringBuilder(arr[i]);
+            a.reverse();
+            sb.append(a);
+            sb.append(" ");
+
+        }
+        return String.valueOf(sb).trim();
+    }
+}
