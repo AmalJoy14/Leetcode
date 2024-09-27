0d7df44da77b1b67dbdd4a06e7cbcf748dd3223f
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-09-27 23:19:31 +0530

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,18 +1,41 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer,Integer> map = new HashMap<>();
+        /*************Moore's Algorithm*************/
+        int freq = 0,element = 0;
         for (int i = 0 ; i < nums.length ; i++){
-            if (!map.containsKey(nums[i])){
-                map.put(nums[i],1);
-            } else {
-                map.put(nums[i],map.get(nums[i])+1);
+            if (freq == 0){
+                element = nums[i];
             }
-        }
-        for (Map.Entry <Integer,Integer> e : map.entrySet()){
-            if (e.getValue() > nums.length/2){
-                return e.getKey();
+            if (nums[i] == element){
+                freq++;
             }
+            else{
+                freq--;
+            }
+            
         }
-        return -1;
+        return element;
     }
 }
+
+
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap <Integer,Integer> map = new HashMap<>();
+//         for (int i = 0 ; i < nums.length ; i++){
+//             if (!map.containsKey(nums[i])){
+//                 map.put(nums[i],1);
+//             } else {
+//                 map.put(nums[i],map.get(nums[i])+1);
+//             }
+//         }
+//         for (Map.Entry <Integer,Integer> e : map.entrySet()){
+//             if (e.getValue() > nums.length/2){
+//                 return e.getKey();
+//             }
+//         }
+//         return -1;
+//     }
+// }
