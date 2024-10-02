8b0d0b9eb4219dbaed17fcef1dc447bdcb939407
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-10-02 18:53:13 +0530

    Update SameTree.java

--- SameTree.java
+++ SameTree.java
@@ -14,21 +14,13 @@
  * }
  */
 class Solution {
-    public void preorder(TreeNode n, List<Integer> list){
-        if (n == null){
-            list.add(null);
-            return;
-        }
-        list.add(n.val);
-        preorder(n.left, list);
-        preorder(n.right, list);
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        preorder(p, list1);
-        preorder(q, list2);
-        // System.out.println(list1 +"\n" + list2);
-        return list1.equals(list2);
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
