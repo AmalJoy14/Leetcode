2c61de67bc45efd427f3a0196d89143039ec8885
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-11-11 10:03:33 +0530

    Update NextPermutation.java

--- NextPermutation.java
+++ NextPermutation.java
@@ -4,35 +4,44 @@ class Solution {
             return;
         int j = nums.length - 2;
         int i, pivot = -1;
+
+        /*
+         * 1)Finding the pivot (We cannot find the next permutation from the elements after pivot
+         *                      as they are already in decreasing order ie.,they form the largest value)
+         */
         for (i = nums.length - 1; j >= 0; i--) {
             if (nums[j] < nums[i]) {
                 pivot = j;
-
-                int nextGreaterIndex = nums.length - 1;
-                for (int k = pivot ; k < nums.length ; k++){
-                    if (nums[k] > nums[pivot]){
-                        nextGreaterIndex = k;
-                    }
-                }
-                
-
-                int temp = nums[pivot];
-                nums[pivot] = nums[nextGreaterIndex];
-                nums[nextGreaterIndex] = temp;
                 break;
             }
             j--;
         }
 
-        //Sorting them in ascending order.
-        for (int k = pivot + 1 ; k < nums.length ; k++){
-            for (int l = k + 1 ; l < nums.length ; l++){
-                if (nums[l] < nums[k]){
-                    int temp = nums[k];
-                    nums[k] = nums[l];
-                    nums[l] = temp;
-                }
+        // 2) Finding the minimum possible element greater than pivot element.
+        int nextGreaterIndex = -1;
+        for (int k = pivot; pivot != -1 && k < nums.length; k++) {
+            if (nums[k] > nums[pivot]) {
+                nextGreaterIndex = k;
             }
+        }
+
+        // 3) Swapping pivot element with next Greater Element.
+        if (pivot != -1){
+            int temp = nums[pivot];
+            nums[pivot] = nums[nextGreaterIndex];
+            nums[nextGreaterIndex] = temp;
+        }
+        
+
+        // 4) Sorting elements after pivot in ascending order.
+        int l = nums.length - 1;
+        int k = pivot + 1;
+        while(k < l){
+            int temp = nums[k];
+            nums[k] = nums[l];
+            nums[l] = temp;    
+            k++;
+            l--;
         }
     }
 }
