2a7d30727adf1ade898ebdd439c0e4dda76af4f4
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2025-01-07 22:39:49 +0530

    Create StringMatchingInAnArray.java

---
+++ StringMatchingInAnArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0 ; i < words.length ; i++){
+            for(int j = 0 ; j < words.length ; j++){
+                if(i == j) continue;
+                if(words[j].contains(words[i]) && !list.contains(words[i])){
+                    list.add(words[i]);
+                }
+            }
+        }
+        return list;
+    }
+}
