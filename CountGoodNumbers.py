4d5760e5b95c75c4bbd7a4837728f193a4c8878d
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2025-04-13 10:50:58 +0530

    Create CountGoodNumbers.py

---
+++ CountGoodNumbers.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+        evens = (n + 1) // 2  # ceil(n / 2)
+        odds = n // 2         # floor(n / 2)
+        return (pow(5, evens, MOD) * pow(4, odds, MOD)) % MOD
