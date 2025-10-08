69155aab7994ad63c0613df423101fe959c786b6
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2025-10-08 11:35:03 +0530

    Create SuccessfulPairsOfSpellsAndPotions.java

---
+++ SuccessfulPairsOfSpellsAndPotions.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length , m = potions.length;
+        int[] resArr = new int[n];
+        Arrays.sort(potions);
+        for(int i = 0 ; i < n ; i++){
+            int left = 0 , right = m - 1 , res = -1;
+            while(left <= right){
+                int mid = left + (right - left) / 2;
+                if(spells[i] * (long)potions[mid] >= success){
+                    res = mid;
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+            if(res != -1){
+                resArr[i] = m - res;
+            }
+        }
+        return resArr;
+    }
+}
