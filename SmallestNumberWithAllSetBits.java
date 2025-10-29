2b8527a57f4361a4cc23882ebb9630ed6b79186e
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2025-10-29 23:24:38 +0530

    Implement method to find smallest number with set bits

---
+++ SmallestNumberWithAllSetBits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int smallestNumber(int n) {
+        int res = 1;
+        while(res < n){
+            res <<= 1;
+            res = res | 1;
+        }
+        return res;
+    }
+}
